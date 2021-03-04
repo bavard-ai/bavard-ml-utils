946810c6c86b3b6f9bb6e5ceffd85560355fd109
Evan Peterson <evanpeterson17@gmail.com>
2021-03-04 13:22:41 -0700

    Add error reporting util

---
+++ bavard_ml_common/mlops/errors.py
@@ -0,0 +1,31 @@
+import typing as t
+import requests
+from google.cloud import error_reporting
+from loguru import logger
+
+
+error_types = {'ERROR', 'MINOR', 'MAJOR', 'CRITICAL'}
+
+
+def report_error(
+    message: str,
+    slack_url: t.Optional[str],
+    gcp_project_id: t.Optional[str],
+    severity: str = 'ERROR',
+):
+    """Reports errors on Slack || Google Cloud || Both
+    """
+    assert severity in error_types
+    error = f":warning: {severity}: {message}"
+
+    if slack_url is not None:
+        try:
+            requests.post(slack_url, data=error)  # report on slack
+        except Exception:
+            logger.exception("Error Reporting: Slack")
+
+    if gcp_project_id is not None:
+        try:
+            error_reporting.Client(gcp_project_id).report(error)  # report on GCP
+        except Exception:
+            logger.exception("Error Reporting: Google")
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 from setuptools.command.install import install
 
 # The version of this package
-VERSION = "0.0.18"
+VERSION = "0.0.19"
 
 
 class VerifyVersionCommand(install):
@@ -49,8 +49,11 @@ setuptools.setup(
         "pydantic==1.7.3",
         "google-cloud-storage==1.35.1",
         "google-cloud-pubsub==2.2.0",
+        "google-cloud-error-reporting==1.1.1",
         "numpy>=1.15.4",
-        "scikit-learn>=0.20.3"
+        "scikit-learn>=0.20.3",
+        "requests>=2.21.0",
+        "loguru>=0.5.1"
     ],
     cmdclass={"verify": VerifyVersionCommand},
 )

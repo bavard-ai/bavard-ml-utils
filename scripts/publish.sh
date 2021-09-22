950f774ae9b57f57c66a5c97898ed5623f047f6a
Evan Peterson <evanpeterson17@gmail.com>
2021-09-22 12:25:49 -0600

    Install poetry before using it anywhere

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,12 +5,13 @@ set -eo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR/..  # go to project root
 
+# Install poetry.
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
+
 pkg_version="$(poetry version --short)"
 if [pkg_version -ne $CIRCLE_TAG]; then
   echo "Git tag: $CIRCLE_TAG does not match the version of this package: $pkg_version"
 fi
 
-# Install poetry.
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
 # Publish the package on pypi.
 poetry publish --build --username __token__ --password $PYPI_PASSWORD

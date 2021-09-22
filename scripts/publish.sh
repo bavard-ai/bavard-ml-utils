c6b3ff4bbba1e845f575aaed5378cc5ececb60d3
Evan Peterson <evanpeterson17@gmail.com>
2021-09-22 12:15:57 -0600

    Install poetry before using it

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -10,4 +10,7 @@ if [pkg_version -ne $CIRCLE_TAG]; then
   echo "Git tag: $CIRCLE_TAG does not match the version of this package: $pkg_version"
 fi
 
+# Install poetry.
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
+# Publish the package on pypi.
 poetry publish --build --username __token__ --password $PYPI_PASSWORD

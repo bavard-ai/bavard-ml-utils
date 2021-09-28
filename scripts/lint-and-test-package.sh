127bb128dc3d4be7e6fe2065729fab36d0fc7dda
Evan Peterson <evanpeterson17@gmail.com>
2021-09-28 16:24:35 -0600

    Use BuildKit for faster Docker builds

--- scripts/lint-and-test-package.sh
+++ scripts/lint-and-test-package.sh
@@ -14,16 +14,17 @@ pre-commit run flake8 --all-files
 
 # Run the unit tests
 
+export DOCKER_BUILDKIT=1
+export COMPOSE_DOCKER_CLI_BUILD=1
 # Needed for emulators to work, see:
 # https://github.com/openssl/openssl/issues/5845#issuecomment-378601109
 export LD_LIBRARY_PATH=/usr/local/lib
 
 cd dockerfiles
 
+docker-compose -f docker-compose.yml build --progress=plain
+
 docker-compose -f docker-compose.yml up \
-  --build \
-  --renew-anon-volumes \
-  --force-recreate \
   --abort-on-container-exit \
   --exit-code-from test
 

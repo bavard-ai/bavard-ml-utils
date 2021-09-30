a2f452f4c5fcaeec13f3921c051c82b1d71fb102
Evan Peterson <evanpeterson17@gmail.com>
2021-09-30 09:16:29 -0600

    Add GCP logging util

---
+++ bavard_ml_utils/gcp/logging.py
@@ -0,0 +1,64 @@
+import json
+import typing as t
+from enum import Enum
+
+
+class Severity(Enum):
+    """
+    GCP Logging log severity levels.
+    Source: https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#FIELDS.trace
+    """
+
+    DEFAULT = "DEFAULT"  # The log entry has no assigned severity level.
+    DEBUG = "DEBUG"  # Debug or trace information.
+    INFO = "INFO"  # Routine information, such as ongoing status or performance.
+    NOTICE = "NOTICE"  # Normal but significant events, such as start up, shut down, or a configuration change.
+    WARNING = "WARNING"  # Warning events might cause problems.
+    ERROR = "ERROR"  # Error events are likely to cause problems.
+    CRITICAL = "CRITICAL"  # Critical events cause more severe problems or outages.
+    ALERT = "ALERT"  # A person must take an action immediately.
+    EMERGENCY = "EMERGENCY"  # One or more systems are unusable.
+
+
+def log(
+    severity: t.Union[Severity, str] = "DEFAULT",
+    *,
+    trace_header: t.Optional[str] = None,
+    gcp_project_id: t.Optional[str] = None,
+    logger: t.Callable[[str], t.Any] = print,
+    **fields,
+):
+    """
+    Logs `**fields` out as a JSON object string. GCP Cloud Run automatically parses the fields, and also captures
+    any special fields, like `severity` and `message`.
+
+    Parameters
+    ----------
+    severity : str or Severity, optional
+        The severity of the log entry.
+    trace_header : str, optional
+        If this log entry is being logged in the context of an HTTP request and response occuring in GCP Cloud Run, then
+        the request will have a `X-Cloud-Trace-Context` header. If the value of that header is passed to this function
+        as the `trace_header` field, this log entry will be associated with that trace, and the entry's fields will be
+        nested under the trace's log entry. *Note*: the `gcp_project_id` field must also be passed in.
+    gcp_project_id : str, optional
+        If applicable, the ID of the GCP project the code is running under. Required for this log entry to be associated
+        with a trace.
+    logger : callable, optional
+        The function that will do the logging to the console. Default is the builtin `print`, which is sufficient for
+        GCP to capture the log entry.
+    """
+    if isinstance(severity, str):
+        # Validate severity.
+        severity = Severity(severity)
+    if len(fields) == 0:
+        # No data was passed in to log, so its a noop.
+        return False
+    log_entry = {"severity": severity.value}
+    if trace_header and gcp_project_id:
+        # Source: https://cloud.google.com/run/docs/logging#writing_structured_logs
+        trace = trace_header.split("/")
+        log_entry["logging.googleapis.com/trace"] = f"projects/{gcp_project_id}/traces/{trace[0]}"
+    log_entry.update(fields)
+    logger(json.dumps(log_entry))
+    return True
---
+++ test/gcp/test_logging.py
@@ -0,0 +1,51 @@
+import json
+from unittest import TestCase
+from uuid import uuid4
+
+from bavard_ml_utils.gcp.logging import Severity, log
+
+
+class TestLogger:
+    def __init__(self):
+        self.call_count = 0
+        self.logged = None
+
+    def __call__(self, body: str):
+        self.call_count += 1
+        self.logged = body
+
+    def reset(self):
+        self.call_count = 0
+        self.logged = None
+
+
+class TestLogging(TestCase):
+    def setUp(self) -> None:
+        self.trace_id = uuid4()
+        # Has the format `TRACE_ID/SPAN_ID;o=TRACE_TRUE`. Source: https://cloud.google.com/trace/docs/setup#force-trace
+        self.trace_header = f"{self.trace_id}/1;o=1"
+        self.gcp_project_id = "test"
+        self.logger = TestLogger()
+
+    def test_can_log(self):
+        self.assertFalse(log())  # logging nothing should be a noop, and return false
+        self.assertTrue(log(message="testing"))
+
+    def test_log_severity(self):
+        with self.assertRaises(ValueError):
+            log(severity="foo")  # invalid severity
+        self.assertTrue(log(severity="DEBUG", message="hello", logger=self.logger))  # can accept strings
+        self.assertTrue(log("ERROR", message="world", logger=self.logger))  # argument name is optional
+        self.assertTrue(log(severity=Severity.INFO, message="hello", logger=self.logger))  # or enum values
+        self.assertEqual(self.logger.call_count, 3)
+        # The message should be present in the log entry body.
+        self.assertEqual(json.loads(self.logger.logged)["message"], "hello")
+
+    def test_links_with_trace(self):
+        self.assertTrue(
+            log(trace_header=self.trace_header, gcp_project_id=self.gcp_project_id, foo="bar", logger=self.logger)
+        )
+        self.assertEqual(self.logger.call_count, 1)
+        self.assertIsNotNone(self.logger.logged)
+        data = json.loads(self.logger.logged)
+        self.assertEqual(data["logging.googleapis.com/trace"], f"projects/{self.gcp_project_id}/traces/{self.trace_id}")
